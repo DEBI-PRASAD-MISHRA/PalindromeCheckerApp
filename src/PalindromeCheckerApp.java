d6350a4a34880841cb9564c3f70635fe1760f71e
DEBI-PRASAD-MISHRA <joojooofpuri@gmail.com>
2026-02-18 11:09:48 +0530

    UC2 ; Hardcoded Palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,9 +1,23 @@
 
 public class PalindromeCheckerApp {
-     public static void main() {
-         System.out.println("Welcome to Palindrome Checer Management System");
+     public static void main (String[] args){
+         System.out.println("Welcome to Palindrome Cheker Management System");
          System.out.println("Version 1.0");
          System.out.println("System Initialized Succesfully");
+         int o=0;
+         String input="madam";
+         for (int i = 0; i < input.length() / 2; i++) {
+             if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                 System.out.println("False");
+                 o=1;
+                 break;
+
+             }
+
+         }
+         if (o==0){
+             System.out.println("True");
+         }
 
      }
 }
